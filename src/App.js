cf32cbff18df3b9cd4de71a8094c7534815aba77
Josue <yoshuah809@gmail.com>
2022-10-04 13:52:36 -0500

    redirection worked on user and admin login instance

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import UserDashboard from "./components/UserDashboard";
+import AdminDashboard from "./components/AdminDashboard";
 import "./App.css";
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
 				<Route index element={<Home />} />
 				<Route path="/signin" element={<SignIn />} />
 				<Route path="/signup" element={<SignUp />} />
+				<Route path="user/dashboard" element={<UserDashboard />} />
+				<Route path="admin/dashboard" element={<AdminDashboard />} />
+
 				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
